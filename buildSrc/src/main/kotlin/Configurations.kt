19b24178406d957e48cff027e3feb06eb3dc5a9e
qiaotong <saki.log@qq.com>
2024-08-12 21:05:43 +0800

    rm .idea

--- buildSrc/src/main/kotlin/Configurations.kt
+++ buildSrc/src/main/kotlin/Configurations.kt
@@ -2,8 +2,8 @@ object Configurations {
     const val targetSdk = 34
     const val midSdk = 26
     const val majorVersion = 1
-    const val minorVersion = 1
-    const val patchVersion = 0
+    const val minorVersion = 2
+    const val patchVersion = 1
     const val versonName = "$majorVersion.$minorVersion.$patchVersion"
     const val versionCode = 1
 }
