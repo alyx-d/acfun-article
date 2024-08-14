1d70dcabf3d91585879df406aeabd5d3f1588ceb
qiaotong <saki.log@qq.com>
2024-08-15 00:07:07 +0800

    background

--- app/src/main/kotlin/com/qt/app/ui/article/PageLoading.kt
+++ app/src/main/kotlin/com/qt/app/ui/article/PageLoading.kt
@@ -2,9 +2,11 @@ package com.qt.app.ui.article
 
 import android.content.Context
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,7 +18,8 @@ import com.qt.app.util.Util.gifLoader
 
 @Composable
 fun PageLoading(context: Context) {
-    Box(modifier = Modifier.fillMaxSize()) {
+    Box(modifier = Modifier.fillMaxSize()
+        .background(MaterialTheme.colorScheme.background)) {
         Image(
             modifier = Modifier
                 .size(150.dp)
