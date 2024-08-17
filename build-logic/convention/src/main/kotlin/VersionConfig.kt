293f0a9e41fa53d4683c178e22bd58d3b6d81672
qiaotong <saki.log@qq.com>
2024-08-18 03:42:08 +0800

    version

--- build-logic/convention/src/main/kotlin/VersionConfig.kt
+++ build-logic/convention/src/main/kotlin/VersionConfig.kt
@@ -3,7 +3,7 @@ object VersionConfig {
     const val midSdk = 26
     const val majorVersion = 1
     const val minorVersion = 1
-    const val patchVersion = 0
+    const val patchVersion = 1
     const val versonName = "$majorVersion.$minorVersion.$patchVersion"
     const val versionCode = 1
 }
