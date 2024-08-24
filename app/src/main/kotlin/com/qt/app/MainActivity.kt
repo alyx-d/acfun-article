bf9eab94af43fc8b76048dad993f6294d7c9d1c8
qt <saki.log@qq.com>
2024-08-25 03:33:08 +0800

    format

--- app/src/main/kotlin/com/qt/app/MainActivity.kt
+++ app/src/main/kotlin/com/qt/app/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
@@ -15,7 +14,6 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.media3.common.util.UnstableApi
 import androidx.navigation.compose.rememberNavController
 import com.qt.app.core.utils.SnackBarHostStateHolder
 import com.qt.app.ui.AppBottomNavBar
@@ -26,8 +24,6 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @UnstableApi
-    @ExperimentalMaterial3Api
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
